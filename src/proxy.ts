5147703e6107c20713ac025f1f0124162b5a1d7e
samx774 <samx4060@gmail.com>
2025-10-28 08:35:13 +0300

    protecting the user routes

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,6 +2,8 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 const isProtectedRoute = createRouteMatcher([
     '/studio(.*)',
+    '/playlists(.*)',
+    '/feed/subscribed',
 ]);
 
 export default clerkMiddleware(async (auth, req)=> {
